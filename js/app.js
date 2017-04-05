e4b12b1e000e90ad541683986d3e42a15877fa90
Ola-Alkhateeb <ola.93.alkhateeb@gmail.com>
2017-04-05 14:30:57 +0300

    undo

--- js/app.js
+++ js/app.js
@@ -1,12 +1,3 @@
-var arr = [{"Title":"Fight Club","Year":"1999","Rated":"R","Released":"15 Oct 1999","Runtime":"139 min","Genre":"Drama","Director":"David Fincher","Writer":"Chuck Palahniuk (novel), Jim Uhls (screenplay)","Actors":"Edward Norton, Brad Pitt, Meat Loaf, Zach Grenier","Plot":"An insomniac office worker, looking for a way to change his life, crosses paths with a devil-may-care soap maker, forming an underground fight club that evolves into something much, much more.","Language":"English","Country":"USA, Germany","Awards":"Nominated for 1 Oscar. Another 10 wins & 31 nominations.","Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BMzc1YmU2ZjEtYWIwMC00ZjM3LWI0NTctMDVlNGQ3YmYwMzE5XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg","Metascore":"66","imdbRating":"8.8","imdbVotes":"1,395,665","imdbID":"tt0137523","Type":"movie","Response":"True"},
-{"Title":"Fast and Furious","Year":"1939","Rated":"PASSED","Released":"06 Oct 1939","Runtime":"73 min","Genre":"Comedy, Crime, Mystery","Director":"Busby Berkeley","Writer":"Harry Kurnitz (original screen play)","Actors":"Franchot Tone, Ann Sothern, Ruth Hussey, Lee Bowman","Plot":"Rare book dealers Joel and Garda take a summertime jaunt to the seashore where he becomes involved in a beauty pageant as investor and judge much to her chagrin.","Language":"English","Country":"USA","Awards":"N/A","Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BMjAyNTQ1NjA3Ml5BMl5BanBnXkFtZTgwOTIyNjIxMzE@._V1_SX300.jpg","Metascore":"N/A","imdbRating":"6.2","imdbVotes":"500","imdbID":"tt0031298","Type":"movie","Response":"True"},
-{"Title":"Saw","Year":"2004","Rated":"R","Released":"29 Oct 2004","Runtime":"103 min","Genre":"Horror, Mystery, Thriller","Director":"James Wan","Writer":"Leigh Whannell, James Wan (story), Leigh Whannell (story)","Actors":"Leigh Whannell, Cary Elwes, Danny Glover, Ken Leung","Plot":"Two strangers awaken in a room with no recollection of how they got there or why, and soon discover they are pawns in a deadly game perpetrated by a notorious serial killer.","Language":"English","Country":"USA","Awards":"8 wins & 10 nominations.","Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BMjE4MDYzNDE1OV5BMl5BanBnXkFtZTcwNDY2OTYwNA@@._V1_SX300.jpg","Metascore":"46","imdbRating":"7.7","imdbVotes":"307,994","imdbID":"tt0387564","Type":"movie","Response":"True"},
-{"Title":"A Walk to Remember","Year":"2002","Rated":"PG","Released":"25 Jan 2002","Runtime":"101 min","Genre":"Drama, Romance","Director":"Adam Shankman","Writer":"Nicholas Sparks (novel), Karen Janszen (screenplay)","Actors":"Shane West, Mandy Moore, Peter Coyote, Daryl Hannah","Plot":"The story of two North Carolina teens, Landon Carter and Jamie Sullivan, who are thrown together after Landon gets into trouble and is made to do community service.","Language":"English","Country":"USA","Awards":"4 wins & 2 nominations.","Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BMzU3NTYxM2MtNjViMS00YmNlLWEwM2MtYWI2MzgzNTkxODFjXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg","Metascore":"35","imdbRating":"7.4","imdbVotes":"167,598","imdbID":"tt0281358","Type":"movie","Response":"True"},
-{"Title":"The Lord of the Rings: The Fellowship of the Ring","Year":"2001","Rated":"PG-13","Released":"19 Dec 2001","Runtime":"178 min","Genre":"Adventure, Drama, Fantasy","Director":"Peter Jackson","Writer":"J.R.R. Tolkien (novel), Fran Walsh (screenplay), Philippa Boyens (screenplay), Peter Jackson (screenplay)","Actors":"Alan Howard, Noel Appleby, Sean Astin, Sala Baker","Plot":"A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle Earth from the Dark Lord Sauron.","Language":"English, Sindarin","Country":"New Zealand, USA","Awards":"Won 4 Oscars. Another 110 wins & 122 nominations.","Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BNTEyMjAwMDU1OV5BMl5BanBnXkFtZTcwNDQyNTkxMw@@._V1_SX300.jpg","Metascore":"92","imdbRating":"8.8","imdbVotes":"1,280,467","imdbID":"tt0120737","Type":"movie","Response":"True"},
-{"Title":"The Notebook","Year":"2004","Rated":"PG-13","Released":"25 Jun 2004","Runtime":"123 min","Genre":"Drama, Romance","Director":"Nick Cassavetes","Writer":"Jeremy Leven (screenplay), Jan Sardi (adaptation), Nicholas Sparks (novel)","Actors":"Tim Ivey, Gena Rowlands, Starletta DuPois, James Garner","Plot":"A poor yet passionate young man falls in love with a rich young woman, giving her a sense of freedom, but they are soon separated because of their social differences.","Language":"English","Country":"USA","Awards":"12 wins & 10 nominations.","Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BMTk3OTM5Njg5M15BMl5BanBnXkFtZTYwMzA0ODI3._V1_SX300.jpg","Metascore":"53","imdbRating":"7.9","imdbVotes":"408,984","imdbID":"tt0332280","Type":"movie","Response":"True"},
-{"Title":"Twilight","Year":"2008","Rated":"PG-13","Released":"21 Nov 2008","Runtime":"122 min","Genre":"Drama, Fantasy, Romance","Director":"Catherine Hardwicke","Writer":"Melissa Rosenberg (screenplay), Stephenie Meyer (novel)","Actors":"Kristen Stewart, Sarah Clarke, Matt Bushell, Billy Burke","Plot":"A teenage girl risks everything when she falls in love with a vampire.","Language":"English","Country":"USA","Awards":"28 wins & 16 nominations.","Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BMTQ2NzUxMTAxN15BMl5BanBnXkFtZTcwMzEyMTIwMg@@._V1_SX300.jpg","Metascore":"56","imdbRating":"5.2","imdbVotes":"356,164","imdbID":"tt1099212","Type":"movie","Response":"True"},
-{"Title":"Death Race","Year":"2008","Rated":"R","Released":"22 Aug 2008","Runtime":"105 min","Genre":"Action, Sci-Fi, Thriller","Director":"Paul W.S. Anderson","Writer":"Paul W.S. Anderson (screenplay), Paul W.S. Anderson (screen story), Robert Thom, Charles B. Griffith, Ib Melchior","Actors":"Jason Statham, Joan Allen, Ian McShane, Tyrese Gibson","Plot":"Ex-con Jensen Ames is forced by the warden of a notorious prison to compete in our post-industrial world's most popular sport: a car race in which inmates must brutalize and kill one another on the road to victory.","Language":"English","Country":"USA, Germany, UK","Awards":"1 win & 5 nominations.","Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BMTkwMDU1ODg5Ml5BMl5BanBnXkFtZTcwNjMzNjY3MQ@@._V1_SX300.jpg","Metascore":"43","imdbRating":"6.4","imdbVotes":"170,585","imdbID":"tt0452608","Type":"movie","Response":"True"}]
-
 
 angular.module('myApp', [])
   .controller('MovieController', function($scope, $http){
@@ -14,30 +5,23 @@ angular.module('myApp', [])
       fetch();
     });
 
-    $scope.search = arr[0].Title.toLowerCase();
+    $scope.search = "Sherlock Holmes";
 
     function fetch(){
       var x = $scope.search.split(' ').join('+');
       $http.get("http://www.omdbapi.com/?t=" + x+ "&y=&plot=short&r=json")
-      // .then(function(response){
-      //   $scope.details = response.data; });
+      .then(function(response){ console.log(response.data)
+        $scope.details = response.data; });
 
-    .then(function (response) {
-      console.log(response.data)
-       $scope.details = response.data;
-  // }, function errorCallback() {
-  //   console.log('error is starting')
-  //     for (var i = 0; i < arr.length; i++) {
-  //       if(arr[i].Title.toLowerCase() === $scope.search){
-  //          $scope.details = arr[i].Title
-  //       }
-  //     }
-  });
+      // $http.get("http://www.omdbapi.com/?s=" + $scope.search)
+      // .then(function(response){ $scope.related = response.data; });
     }
 
     $scope.update = function(movie){
       $scope.search = movie.Title;
     };
 
-
+    $scope.select = function(){
+      this.setSelectionRange(0, this.value.length);
+    }
   });
